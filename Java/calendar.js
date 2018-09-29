dd2b490c2f0aed3f45b7e36af46ec247e8d1ac20
KennethPryor <kennypryor131@gmail.com>
2018-09-29 09:45:35 -0400

    fixed format issue and made td for dates

--- Java/calendar.js
+++ Java/calendar.js
@@ -1,31 +1,30 @@
 $(document).ready(function () {
-    //var for days of week
-    const Sunday = moment().startOf('week').toString();
-    const Monday = moment().startOf('week').add(1, 'day').toString();
-    const Tuesday = moment().startOf('week').add(2, 'day').toString();
-    const Wednesday = moment().startOf('week').add(3, 'day').toString();
-    const Thursday = moment().startOf('week').add(4, 'day').toString();
-    const Friday = moment().startOf('week').add(5, 'day').toString()
-    const Saturday = moment().endOf('week').toString();
+
+    const Sunday = moment().startOf('week').format('ddd MM Do');
+    const Monday = moment().startOf('week').add(1, 'day').format('ddd MM Do');
+    const Tuesday = moment().startOf('week').add(2, 'day').format('ddd MM Do');
+    const Wednesday = moment().startOf('week').add(3, 'day').format('ddd MM Do');
+    const Thursday = moment().startOf('week').add(4, 'day').format('ddd MM Do');
+    const Friday = moment().startOf('week').add(5, 'day').format('ddd MM Do')
+    const Saturday = moment().endOf('week').format('ddd MM Do');
     const CalDays = [Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday];
-    
-    
-    // const Today = moment().format('ddd MMM Do')
-    // const Tomorrow =  moment().add(1, 'day').format('ddd MMM Do')
-    // const MomentToday;
     const MomentCalFormat = moment().format('ddd MM Do');
-    //for each day of the week, add a td for in the calendar 
+
     CalDays.forEach(function(Day) {
         
-        const CalDaysTd = $(`
+        const CalDaysTh = $(`
         <th>${Day}</th>
         `);
+        const CalDaysTd = $(`
+        <td class="${Day}"></td>
+        `);
+        $(CalDaysTh).appendTo("#CalDaysTh");
         $(CalDaysTd).appendTo("#CalDaysTd");
+
     });
-    //momentjs the week so calandar knows the dates
-    
 
     console.log(Sunday);
     console.log(Monday);
     console.log(Saturday);
+    console.log(MomentCalFormat)
 });
