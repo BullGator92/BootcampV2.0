a594fd3682217c60e74ee03317db90d4924980b1
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-10-02 09:50:38 -0400

    Update main.js

--- Java/main.js
+++ Java/main.js
@@ -2,7 +2,7 @@
    function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 13,
-      center: {lat: 34.04924594193164, lng: -118.24104309082031}
+      center: {lat: 33.77661467620815, lng: -84.39008474349977}
     });
 }
     var trafficLayer = new google.maps.TrafficLayer();
