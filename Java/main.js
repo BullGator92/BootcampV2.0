fb5d36d71fbe41cf8bd0867b2e4a251c82a324ff
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-10-03 10:03:09 -0400

    Update main.js

--- Java/main.js
+++ Java/main.js
@@ -1,10 +1,4 @@
    //Google Maps traffic layer
-   function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: {lat: 33.77661467620815, lng: -84.39008474349977}
-    });
-}
     var trafficLayer = new google.maps.TrafficLayer();
     trafficLayer.setMap(map);
 
