e5695715f36d7127cbb42db2e5fba65a5ffe87fb
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2018-10-10 14:11:58 -0400

    initial commit

--- Java/main.js
+++ Java/main.js
@@ -1,73 +1,51 @@
-
-//Google Maps traffic layer
-var trafficLayer = new google.maps.TrafficLayer();
-trafficLayer.setMap(map);
-
-infoWindow = new google.maps.InfoWindow;
-
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
+var map, infoWindow;
+function initMap() {
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var mapOptions = {
+        zoom:7,
+        center: {lat: 33.77661467620815, lng: -84.39008474349977}
     };
-    console.log(pos);
-
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    infoWindow.open(map);
-    map.setCenter(pos);
-  }, function () {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directions'));
+    calcRoute(directionsDisplay);
 }
 
+function calcRoute(directionsDisplay) {
+    var end = new google.maps.LatLng(33.77661467620815, -84.39008474349977);
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var start = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: 'DRIVING',
+            drivingOptions: {
+                departureTime: new Date(),
+                trafficModel: 'pessimistic'
+            }
+        };
+
+        new google.maps.DirectionsService().route(request, function(result, status) {
+            if (status == 'OK') {
+                directionsDisplay.setDirections(result);
+            }
+        });
+    });
 }
-//Driving Layer//
-var origin1 = new google.maps.LatLng(pos);
-var destinationB = new google.maps.LatLng(33.77661467620815, -84.39008474349977);
+// TRAFFIC OVERLAY
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 13,
+//     center: {lat: 33.77661467620815, lng: -84.39008474349977}
+//   });
 
-function callback(response, status) {
-  if (status == 'OK') {
-    var origins = response.originAddresses;
-    var destinations = response.destinationAddresses;
-
-    for (var i = 0; i < origins.length; i++) {
-      var results = response.rows[i].elements;
-      for (var j = 0; j < results.length; j++) {
-        var element = results[j];
-        var distance = element.distance.text;
-        var duration = element.duration.text;
-        var from = origins[i];
-        var to = destinations[j];
-      }
-    }
-  }
-}
-
-var service = new google.maps.DistanceMatrixService();
-service.getDistanceMatrix(
-  {
-    origins: [origin1, origin2],
-    destinations: [destinationA, destinationB],
-    travelMode: 'DRIVING',
-    transitOptions: TransitOptions,
-    drivingOptions: DrivingOptions,
-    unitSystem: UnitSystem,
-    avoidHighways: Boolean,
-    avoidTolls: Boolean,
-  }, callback);
+//   var trafficLayer = new google.maps.TrafficLayer();
+//   trafficLayer.setMap(map);
+// }
 
 
 
