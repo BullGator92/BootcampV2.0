3a9e5db5e21e6b5e3cc8d381812857c933225604
KennethPryor <kennypryor131@gmail.com>
2018-10-10 06:12:41 -0400

    reworked html for calendar and added links,changed calendar js for fullcalendar and linked to google calendar,restructured page to look better

--- Java/calendar.js
+++ Java/calendar.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
     $('#calendar').fullCalendar({
+        googleCalendarApiKey: 'AIzaSyA4AwsXZa56jFyvszoiQIjHLE1NggvFm18',
+        events: {
+          googleCalendarId: '2018gatechbootcampsite@gmail.com',
+        },
         defaultView: 'listWeek',
-        height: 600,
-        
-        
-         })
+        height: 620},
     
-    
-    
-});
+    )});
